422523f33da010a42b79f1ff9b87785cd002f365
lorenzobarilla <37076142+lorenzobarilla@users.noreply.github.com>
2020-01-15 18:27:59 +0100

    re-evaluation

--- sketch.js
+++ sketch.js
@@ -1,72 +1,93 @@
-var snowflakes = [];
-var amountOfSnowflakes = 2500;
-var tree;
+// sound global variables
+var noiseSound;
+var punchSound;
+var chefAudio
 
+// images global variables
+var tv;
+var noiseImg;
+var chef;
+var pow;
+var hand;
+
+// punch counter
+var punchCounter = 0;
 
 function preload() {
-  tree = loadImage('tree.png');
+  //load sounds
+  noiseSound = loadSound("./assets/noise.mp3");
+  punchSound = loadSound("./assets/punch.m4a");
+  chefAudio = loadSound("./assets/chef-audio.m4a");
+
+  // load images
+  tv = loadImage("./assets/television.png");
+  noiseImg = loadImage("./assets/noise.png");
+  chef = loadImage("./assets/chef.png");
+  chefNoise = loadImage("./assets/chef-noise.png");
+  pow = loadImage("./assets/pow.png");
+  hand = loadImage("./assets/hand.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
 
-  fill(240);
-  noStroke();
-  for (var i = 0; i < amountOfSnowflakes; i++) {
-    snowflakes.push(new snowflake()); // append snowflake object
-  }
+  // start sound
+  noiseSound.loop();
+  chefAudio.loop();
+
+ // set volumes
+  chefAudio.amp(0);
+  noiseSound.amp(1.0);
 }
 
 function draw() {
-  background("blue");
+  background(0);
 
-  push();
+  // instruction text
+  var instructions = createDiv("Oh no, I can't watch my favourite show! <br> SHAKE your phone to hit the TV and make it work again!");
+  instructions.style('user-select: none; position: absolute; bottom: 10%; left: 50%; transform: translateX(-50%); color: white; text-align: center; font-family: "Source Code Pro"; font-size: 20pt');
+
+  // display images
   imageMode(CENTER);
-  image(tree, width / 2, height - tree.height / 2.5, tree.width / 2.25, tree.height / 2.25);
-  pop();
+  image(noiseImg, width / 2, height * 0.6, noiseImg.width, noiseImg.height);
 
-  let t = frameCount / 60; // update time
+  //tv show appears gradually after some punches
+  if (punchCounter >= 2) {
+    image(chefNoise, width / 2, height * 0.6, chefNoise.width, chefNoise.height);
+  }
+  if (punchCounter >= 4) {
+    image(chef, width / 2, height * 0.6, chef.width, chef.height);
+  }
 
-  for (var i = 0; i < snowflakes.length; i++) {
-    var tempSnowflake = snowflakes[i];
-    tempSnowflake.update(t); // update snowflake position
-    tempSnowflake.display(); // draw snowflake
+  image(tv, width / 2, height * 0.6, tv.width, tv.height);
+  image(hand, width * 0.3, height * 0.6 - tv.height + accelerationZ, hand.width, hand.height);
+
+  // shaking interaction
+  if (accelerationZ >= 10) {
+    image(pow, width / 2, height * 0.6 - tv.height / 2, pow.width, pow.height);
+    punchSound.play();
+    punchCounter++;
+  }
+
+  // increase voice volume and reduce noise volume after every punch
+  var chefVolume = map(punchCounter, 1, 5, 0.1, 1.0);
+  var noiseVolume = map(punchCounter, 1, 5, 1.0, 0.2)
+  chefAudio.amp(chefVolume);
+  noiseSound.amp(noiseVolume);
+
+  // after some punches open a link to the show
+  if (punchCounter >= 5) {
+    window.open("https://www.youtube.com/watch?v=Sj4FVErXDJA", "_self");
   }
 }
 
-function snowflake() {
-  // initialize coordinates
-  this.posX = 0;
-  this.posY = random(-2500, height + 5000);
-  this.initialangle = random(0, 2 * PI);
-  this.size = random(2, 5);
 
-  // radius of snowflake spiral
-  // chosen so the snowflakes are uniformly spread out in area
-  this.radius = sqrt(random(pow(width / 2, 2)));
-
-  this.update = function(time) {
-
-    // x position follows a circle
-    let w = 0.6; // angular speed
-    let angle = w * time + this.initialangle;
-    this.posX = width / 2 + this.radius * sin(angle);
-
-    // different size snowflakes fall at slightly different y speeds
-    rotateIncrementX = 0.25 * map(rotationX, -180, 180, -15, 15);
-    this.posY += rotateIncrementX * pow(this.size, 0.5);
-  };
-
-  this.display = function() {
-    ellipse(this.posX, this.posY, this.size);
-  };
+// prevent dragging the screen around
+function touchMoved() {
+  return false;
 }
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
-}
-
-function mousePressed() {
-  var fs = fullscreen();
-  fullscreen(!fs);
+// iOS 13 request to access motion and orientation data
+function touchEnded(event) {
+  DeviceOrientationEvent.requestPermission()
 }
